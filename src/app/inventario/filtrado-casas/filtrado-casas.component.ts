2c3cbdb8acea539dc8a3913978978e66e01a2723
Alexis Suarez <a219201031@unison.mx>
2023-11-22 14:39:49 -0700

    avance en componentes de inventario

--- src/app/inventario/filtrado-casas/filtrado-casas.component.ts
+++ src/app/inventario/filtrado-casas/filtrado-casas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-filtrado-casas',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./filtrado-casas.component.scss']
 })
 export class FiltradoCasasComponent {
+
+  @Input() mensaje: string = '';
 
 }
