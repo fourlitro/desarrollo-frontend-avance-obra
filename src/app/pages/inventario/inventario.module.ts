c5ec85a62c13fbe7d2d085384a7c4345f30ed474
Alexis Suarez <a219201031@unison.mx>
2023-11-22 15:52:52 -0700

    se agrego servicio datos con datos estaticos

--- src/app/pages/inventario/inventario.module.ts
+++ src/app/pages/inventario/inventario.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DetalleCasasComponent } from './detalle-casas/detalle-casas.component';
+
+import { CasasService } from 'src/app/services/casas.service';
+
 import { ExploradorCasasComponent } from './explorador-casas/explorador-casas.component';
 import { FiltradoCasasComponent } from './filtrado-casas/filtrado-casas.component';
 import { ListadoCasasComponent } from './listado-casas/listado-casas.component';
+import { DetalleCasasComponent } from './detalle-casas/detalle-casas.component';
 
 
 
 @NgModule({
   declarations: [
-    DetalleCasasComponent,
     ExploradorCasasComponent,
     FiltradoCasasComponent,
-    ListadoCasasComponent
+    ListadoCasasComponent,
+    DetalleCasasComponent
   ],
   imports: [
     CommonModule
+  ],
+  providers:[
+    CasasService,
   ],
   exports: [
     ExploradorCasasComponent
