3d2047296015d5675f026991712db58650ef43d9
Alexis Suarez <a219201031@unison.mx>
2023-11-22 14:17:27 -0700

    agregar componentes a modulo inventario

---
+++ src/app/inventario/filtrado-casas/filtrado-casas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filtrado-casas',
+  templateUrl: './filtrado-casas.component.html',
+  styleUrls: ['./filtrado-casas.component.scss']
+})
+export class FiltradoCasasComponent {
+
+}
--- src/app/inventario/inventario.module.ts
+++ src/app/inventario/inventario.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DetalleCasasComponent } from './detalle-casas/detalle-casas.component';
+import { ExploradorCasasComponent } from './explorador-casas/explorador-casas.component';
+import { FiltradoCasasComponent } from './filtrado-casas/filtrado-casas.component';
+import { ListadoCasasComponent } from './listado-casas/listado-casas.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetalleCasasComponent,
+    ExploradorCasasComponent,
+    FiltradoCasasComponent,
+    ListadoCasasComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ExploradorCasasComponent
   ]
 })
 export class InventarioModule { }
